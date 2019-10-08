5b3329b98dd52b67445d986b2b43173960580dd3
Chanaka Weerasinghe <chanaka.weerasinghe@interblocks.com>
2019-10-08 16:06:15 +0530

    Error finalize

--- src/app/services/user-api.service.ts
+++ src/app/services/user-api.service.ts
@@ -16,21 +16,19 @@ export class UserApiService {
   handleError(error: HttpErrorResponse) {
     if (error.error instanceof ErrorEvent) {
       // A client-side or network error occurred. Handle it accordingly.
-      // console.error('An error occurred:', error.error.message);
-      return throwError('An error occurred:'+ error.error.message);
-    } else {
+      console.error('An error occurred:', error.error.message);
+    }else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
-      // console.error(
-      //     `Backend returned code ${error.status}, ` +
-      //     `body was: ${error.error}`);
+      console.error(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`);
     }
     // return an observable with a user-facing error message
     return throwError(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
-  };
+        'Something bad happened; please try again later.');
 
+};
 
   getUsers(): Observable<RootObject> {
     console.error('getUsers() Method Invoked:');
